0bf229ccd769c0da6a8b9285e90a7a9af4bae3ef
Leo Bayer <im@resourcelab.dk>
2019-06-19 18:51:15 +0200

    changed a lot of things

--- _site/assets/js/mediumish.js
+++ _site/assets/js/mediumish.js
@@ -112,5 +112,71 @@ jQuery(document).ready(function($){
     $('.site-content').css('margin-top', $('header').outerHeight() + 'px');
 
 
+    (function($) {
+
+  /*------------------
+    Navigation
+  --------------------*/
+  $('.nav-switch').on('click', function(event) {
+    $('.main-menu').slideToggle(400);
+    event.preventDefault();
+  });
+
+
+
+  /*------------------
+    Background set
+  --------------------*/
+  $('.set-bg').each(function() {
+    var bg = $(this).data('setbg');
+    $(this).css('background-image', 'url(' + bg + ')');
+  });
+
+
+
+  /*----------------------
+    Portfolio layout
+  ------------------------*/
+  var port_fi =  $('.portfolios-area .first-item'),
+    port_si =  $('.portfolios-area .second-item'),
+    port_intro_h =  $('.portfolio-intro').innerHeight();
+
+  if ($(window).width() > 991) {
+    port_fi.appendTo('.portfolio-intro');
+    port_si.find('.portfolio-item').height(port_intro_h + 601);
+  }
+
+  $('.portfolio-item.pi-style2').each(function() {
+    var pi_width = $(this).width();
+    $(this).height(pi_width + 50);
+  });
+
+
+
+  /*------------------
+    Popup
+  --------------------*/
+  $('.portfolio-item').magnificPopup({
+    type: 'image',
+    mainClass: 'img-popup-warp',
+    removalDelay: 400,
+  });
+
+
+  /*------------------
+    Accordions
+  --------------------*/
+  $('.panel-link').on('click', function (e) {
+    $('.panel-link').parent('.panel-header').removeClass('active');
+    var $this = $(this).parent('.panel-header');
+    if (!$this.hasClass('active')) {
+      $this.addClass('active');
+    }
+    e.preventDefault();
+  });
+
+
+})(jQuery);
+
 
 });
